47619abf193f0c6593c21c4d0121dcb7c84a1b6a
Mehrdad-GH <mehrdadghayour7@gmail.com>
2024-08-28 01:59:10 +0330

    fix: Add comment & fix position

--- components/Landing/LearnBlockchain.vue
+++ components/Landing/LearnBlockchain.vue
@@ -1,39 +1,51 @@
 <template>
-  <div class="mb-96">
-    <!-- Marker Box -->
+  <div class="">
+    <!-- Marker Box Component -->
     <ShapeMarkerBox class="mt-28" />
-    <!-- Shadow overlay -->
+    <!-- Shadow Overlay -->
     <div
-    class="absolute bg-[#3D5CFF33] w-96 h-96 mb-10 right-0 translate-x-28 -translate-y-18 blur-2xl rounded-full"
+      class="absolute bg-[#3D5CFF33] w-96 h-96 mb-10 right-0 translate-x-28 -translate-y-18 blur-2xl rounded-full"
     ></div>
-    <!-- Bubble Shape -->
+    <!-- Bubble Shape Component -->
     <ShapeBubbleShape />
   </div>
-    
-  <div class="flex justify-center font-extrabold text-5xl mb-20">
-    <h1>چرا باید  <span class="text-[#7F56D9] font-extrabold">بلاکچین </span>یاد بگیریم؟ </h1>
-  </div>
-  <div class=" mb-96 flex flex-wrap ">
-    <div v-for="item in BlockchainItem" class="lg:w-1/4">
-      <div class="flex justify-between items-center px-10 py-5 my-10 mx-5 rounded-lg duration-100 bg-[#524870] hover:bg-slate-600">
-        <h1 class="font-light">{{ item.name }}  </h1>
-        <UIcon :name="item.Icon" class="w-5 h-5"/>
-      </div>
+
+  <div class="mt-72"> <!-- container -->
+    <!-- Heading -->
+    <div class="flex justify-center font-extrabold text-5xl mb-20">
+      <h1>
+        چرا باید <span class="text-[#7F56D9] font-extrabold">بلاکچین </span>یاد
+        بگیریم؟
+      </h1>
     </div>
 
-  </div>  
-
+    <!-- Blockchain Items -->
+    <div class="flex flex-wrap">
+      <!-- Loop through BlockchainItem -->
+      <div v-for="item in BlockchainItem" class="lg:w-1/4">
+        <!-- Card -->
+        <div
+          class="flex justify-between items-center px-10 py-5 my-10 mx-5 rounded-lg duration-300 bg-[#524870] hover:bg-purple-800"
+        >
+          <h1 class="font-light">{{ item.name }}</h1>
+          <!-- Icon -->
+          <UIcon :name="item.Icon" class="w-5 h-5" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+// Blockchain benefits array
 const BlockchainItem = [
-  {name:"پرسرعت و بدون اتلاف زمان" , Icon:"gravity-ui:picture"},
-  {name:"تکنولوژی آینده" , Icon:"majesticons:scroll-text-line"},
-  {name:"امنیت لایه ای" , Icon:"lucide:layers-3"},
-  {name:"موقعیت های شغلی" , Icon:"mdi:fountain-pen-tip"},
-  {name:"مهاجرت" , Icon:"tabler:music"},
-  {name:"بازار های مالی" , Icon:"charm:chart-line"},
-  {name:"  نو ظهور و دست نخورده" , Icon:"ic:round-incomplete-circle"},
-  {name:"ذخیره داده های بزرگ  " , Icon:"material-symbols:database-outline"},
-]
+  { name: "پرسرعت و بدون اتلاف زمان", Icon: "gravity-ui:picture" },
+  { name: "تکنولوژی آینده", Icon: "majesticons:scroll-text-line" },
+  { name: "امنیت لایه ای", Icon: "lucide:layers-3" },
+  { name: "موقعیت های شغلی", Icon: "mdi:fountain-pen-tip" },
+  { name: "مهاجرت", Icon: "tabler:music" },
+  { name: "بازار های مالی", Icon: "charm:chart-line" },
+  { name: "نو ظهور و دست نخورده", Icon: "ic:round-incomplete-circle" },
+  { name: "ذخیره داده های بزرگ", Icon: "material-symbols:database-outline" },
+];
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@nuxt/ui", "@nuxt/image", "vue3-carousel-nuxt", "nuxt-aos"],
   css :["~/assets/main.css"],
   colorMode: {
