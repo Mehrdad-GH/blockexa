8578f6bcf72f1e1c07fa397be63fea7fd7148271
Mehrdad-GH <mehrdadghayour7@gmail.com>
2024-09-15 13:26:36 +0330

    fix: Add vue3-carousel-nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ["@nuxt/ui", "@nuxt/image", "nuxt-swiper", "nuxt-aos"],
+  modules: ["@nuxt/ui", "@nuxt/image", "nuxt-swiper", "nuxt-aos","vue3-carousel-nuxt"],
   css :["~/assets/main.css"],
   colorMode: {
     preference: 'light'
